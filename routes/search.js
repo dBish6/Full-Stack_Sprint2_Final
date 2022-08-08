0b5654de31391deaed7785e7de9b22aa633dca0d
dBish6 <davidbish2002@hotmail.com>
2022-08-08 20:11:48 -0230

    David August 8 Code Dump

--- routes/search.js
+++ routes/search.js
@@ -1,25 +1,82 @@
+/* server.js
+   List of functions for running the node express HTTP sever on LocalHost
+   with routes for the following URL destinations on the server and for running the home page routes. 
+
+   http://localhost:${PORT}/
+
+   Author: David Bishop, Dominic Whelan, Chris Doucette and Blake Waddleton
+   Creation Date: August 8, 2022
+   Updates:
+   Date, Author, Description
+   Aug 8, 2022, Name, ...
+   
+*/
+
 const express = require("express");
 const router = express.Router();
 
-const {} = require("../model/controllers/m.search.dal");
+const monSearchData = require("../model/controllers/m.search.dal");
+const gresSearchData = require("../model/controllers/p.search.dal");
 
-router.post("/search/postgres", async (req, res) => {
+router.post("/mongo", async (req, res) => {
   try {
-    // He actually used toArray() here for ejs, but we could still probably use json.
-    res.status(200).json();
+    if (DEBUG) console.log(req.body);
+    let mSearch = await monSearchData.fullSearch(req.body.search);
+    // Render this route with m.search.ejs with the fullSearch function.
+    res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
+    // res.redirect("/search/mongo1", { mSearch });
   } catch (error) {
-    res.status(500).json({ error: error.message });
     console.error(error);
+    // Send the 503 status code and render 503.ejs to the user.
+    res.status(503).render("503");
   }
 });
 
-router.post("/search/mongo", async (req, res) => {
+router.post("/postgres", async (req, res) => {
   try {
-    res.status(200).json();
+    let pSearch = await gresSearchData.titleSearch(req.body.search);
+    // Render this route with m.search.ejs with the fullSearch function.
+    res.render("searchResults/p_search", { pSearch, title: "Postgres Search" });
   } catch (error) {
-    res.status(500).json({ error: error.message });
     console.error(error);
+    res.status(503).render("503");
   }
 });
+
+// router.get("/mongo1", async (req, res) => {
+//   try {
+//     // Render this route with m.search.ejs with the fullSearch function.
+//     res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
+//   } catch (error) {
+//     console.error(error);
+//     // Send the 503 status code and render 503.ejs to the user.
+//     res.status(503).render("503");
+//   }
+// });
+
+// router
+//   .post("/mongo", async (req, res) => {
+//     try {
+//       if (DEBUG) console.log(req.body);
+//       let mSearch = await searchData.fullSearch(req.body.search);
+//       // Render this route with m.search.ejs with the fullSearch function.
+//       // router.get("/mongo"),
+//       //   async (req, res) => {
+//       //     res.render("searchResults/m_search", {
+//       //       mSearch,
+//       //       title: "Mongo Search",
+//       //     });
+//       //     // res.redirect("/search/mongo1", { mSearch });
+//       //   };
+//     } catch (error) {
+//       console.error(error);
+//       // Send the 503 status code and render 503.ejs to the user.
+//       res.status(503).render("503");
+//     }
+//   })
+//   .get((req, res) => {
+//     res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
+//     // res.redirect("/search/mongo1", { mSearch });
+//   });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 
    http://localhost:${PORT}/
 
-   Author: 
-   Creation Date: July 13, 2022
+   Author: David Bishop, Dominic Whelan, Chris Doucette and Blake Waddleton
+   Creation Date: August 8, 2022
    Updates:
    Date, Author, Description
    Aug 8, 2022, Name, ...
@@ -14,14 +14,38 @@
 
 const express = require("express");
 const app = express();
-// const morgan = require("morgan");
+const morgan = require("morgan");
 require("dotenv").config();
-
-// const pool = require("./data/db.config");
 
 global.DEBUG = true;
 
 const PORT = process.env.PORT || 4000;
+
+// Setting the view engine to ejs.
+app.set("view engine", "ejs");
+
+// *Middleware*
+// Event Emitters
+app.use(morgan("dev"));
+// So express can use your static files, which is my public folder; css, images, HTML, etc.
+app.use(express.static("public"));
+// So express can read the new perameters off the url and encoding them corrently; we can now use req.body.
+app.use(express.urlencoded({ extended: true }));
+
+// *Posgres Imports*
+const pMovieData = require("./model/controllers/p.films.dal");
+// <Require data.dal here>
+
+// *Mongo Imports*
+const mMovieData = require("./model/controllers/m.movies.dal");
+const searchRouter = require("./routes/search");
+const authRouter = require("./routes/auth");
+
+// *Routers*
+// Mongo Search Router
+app.use("/search", searchRouter);
+// Mongo Auth Router
+app.use("/auth", authRouter);
 
 app.listen(PORT, "localhost", () => {
   console.log(
@@ -30,25 +54,46 @@ app.listen(PORT, "localhost", () => {
   // Could create the database collection for whoever starts the server here; when the server starts up or we can have a command.
 });
 
-// *Middleware*
-// Event Emitters
-// app.use(morgan("dev"));
-app.use(express.static("public"));
-// When getting data from the client; to get json data from req.body.
-app.use(express.json());
-// Probably going to need to use this at some point.
-// app.use(express.urlencoded({ extended: true }));
+app.get("/", async (req, res) => {
+  try {
+    const mMovies = await mMovieData.displayAllMongoMovies();
+    // if (DEBUG) console.log(mMovies);
+    const pMovies = await pMovieData.displayAllPostgresFlims();
+    if (DEBUG) console.log(pMovies);
 
-// *Posgres Imports*
-// <Require data.dal here>
-// *Mongo Imports*
-const searchRouter = require("./routes/search");
-const authRouter = require("./routes/auth");
+    if (mMovies.length === 0) {
+      // Send the 502 status code and render 502.ejs to the user.
+      res.status(502).render("502");
+    } else {
+      // Render this route with home.ejs with the displayAllMongoMovies() and displayAllPostgresMovies().
+      res.render("home", { mMovies, pMovies, title: "Home" });
+    }
+  } catch (error) {
+    console.error(error);
+    // Send the 503 status code and render 503.ejs to the user.
+    res.status(503).render("503");
+  }
+});
 
-// *Routers*
-// Mongo Search Router
-app.use("/api/search", searchRouter);
-// Mongo Auth Router
-app.use("/api/auth", authRouter);
+// app.get("/mongo", async (req, res) => {
+//   try {
+//     const movies = await movieData;
+//     if (DEBUG) console.log(movies);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
-// <Home routes go here>
+// app.get("/postgres", async (req, res) => {
+//   try {
+//     const movies = await movieData;
+//     if (DEBUG) console.log(movies);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// Renders the 404.ejs when there is no GET found; middleware.
+app.use((req, res) => {
+  res.status(404).render("404");
+});
