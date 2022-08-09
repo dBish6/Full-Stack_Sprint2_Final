abab69e6662fdbaae75fe1f7b5acfaf737fbfac4
dBish6 <davidbish2002@hotmail.com>
2022-08-09 19:54:17 -0230

    David August 9 Code Dump

--- model/controllers/p.films.dal.js
+++ model/controllers/p.films.dal.js
@@ -1,39 +1,39 @@
-/* p.search.dal.js
-   Anything to do with the films in the DVD rentals database;
-   getFilmsDisplay() - fetches all the flims in the database.
-   getFilmDetails() - fetches the details of films from the database; description, rating, etc.
+/* p.films.dal.js
+   Anything to do with the films in the DVD rentals database.
+   displayAllPostgresFilms() - gets all the films only with their id, title & year in the database from the film_list view.
+   getFilmDetails() - gets the details of the films in the database by the film_list view; description, rating, etc.
 
    Author: David Bishop
    Creation Date: July 13, 2022
    Updates:
    Date, Author, Description
-   July 13, 2022, David, inital set up; require method.
-   July 14 2022, David, implemented getFilmsDisplay() and getFilmDetails() using async await.
+   August 8, 2022, David; impemented displayAllPostgresFilms() and getFilmDetails().
 
 */
 
 const dal = require("../postgres.db.config");
 
-const displayAllPostgresFlims = async () => {
+const displayAllPostgresFilms = async () => {
   let response;
   try {
     response = await dal.query(
       "SELECT fid, title, release_year FROM film_list ORDER BY random() LIMIT 50;"
     );
     return response.rows;
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
 };
 
+// Gets the film details by whatever id passed in as a parameter.
 const getFilmDetails = async (id) => {
   let response;
   try {
     response = await dal.query("SELECT * FROM film_list WHERE fid = $1;", [id]);
     return response.rows;
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
 };
 
-module.exports = { displayAllPostgresFlims };
+module.exports = { displayAllPostgresFilms };
--- routes/search.js
+++ routes/search.js
@@ -8,43 +8,33 @@
    Creation Date: August 8, 2022
    Updates:
    Date, Author, Description
-   Aug 8, 2022, Name, ...
+   August 8, 2022, David; implemented POST routes, /mongo & /postgres, for the form action on searching using req.body.
+   August 9, 2022, David; changed the two POST routes to GET and used req.query, makes more sense that way.
+   August 9, 2022, David; made a autocomple search for /mongo; was alot of playing around.
    
 */
 
 const express = require("express");
 const router = express.Router();
 
+// Search event logging.
+const EventEmitter = require("events");
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+
+const logEvent = require("../event_emitters/logEvents");
+
+myEmitter.addListener("log", (msg, level, logName) =>
+  logEvent(msg, level, logName)
+);
+
 const monSearchData = require("../model/controllers/m.search.dal");
 const gresSearchData = require("../model/controllers/p.search.dal");
 
-router.post("/mongo", async (req, res) => {
-  try {
-    if (DEBUG) console.log(req.body);
-    let mSearch = await monSearchData.fullSearch(req.body.search);
-    // Render this route with m.search.ejs with the fullSearch function.
-    res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
-    // res.redirect("/search/mongo1", { mSearch });
-  } catch (error) {
-    console.error(error);
-    // Send the 503 status code and render 503.ejs to the user.
-    res.status(503).render("503");
-  }
-});
-
-router.post("/postgres", async (req, res) => {
-  try {
-    let pSearch = await gresSearchData.titleSearch(req.body.search);
-    // Render this route with m.search.ejs with the fullSearch function.
-    res.render("searchResults/p_search", { pSearch, title: "Postgres Search" });
-  } catch (error) {
-    console.error(error);
-    res.status(503).render("503");
-  }
-});
-
-// router.get("/mongo1", async (req, res) => {
+// router.get("/mongo", async (req, res) => {
 //   try {
+//     if (DEBUG) console.log(req.query);
+//     let mSearch = await monSearchData.fullSearch(req.query.search);
 //     // Render this route with m.search.ejs with the fullSearch function.
 //     res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
 //   } catch (error) {
@@ -54,29 +44,61 @@ router.post("/postgres", async (req, res) => {
 //   }
 // });
 
-// router
-//   .post("/mongo", async (req, res) => {
-//     try {
-//       if (DEBUG) console.log(req.body);
-//       let mSearch = await searchData.fullSearch(req.body.search);
-//       // Render this route with m.search.ejs with the fullSearch function.
-//       // router.get("/mongo"),
-//       //   async (req, res) => {
-//       //     res.render("searchResults/m_search", {
-//       //       mSearch,
-//       //       title: "Mongo Search",
-//       //     });
-//       //     // res.redirect("/search/mongo1", { mSearch });
-//       //   };
-//     } catch (error) {
-//       console.error(error);
-//       // Send the 503 status code and render 503.ejs to the user.
-//       res.status(503).render("503");
-//     }
-//   })
-//   .get((req, res) => {
-//     res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
-//     // res.redirect("/search/mongo1", { mSearch });
-//   });
+router.get("/mongo", async (req, res) => {
+  try {
+    if (DEBUG) console.log(req.query);
+    // Autocomplete search or fuzzy search by the search index in the mongoDb movie collection on title.
+    let auto = collection
+      .aggregate([
+        {
+          $search: {
+            autocomplete: {
+              query: `${req.query.search}`,
+              path: "title",
+              fuzzy: {
+                maxEdits: 2,
+              },
+            },
+          },
+        },
+      ])
+      .limit(30);
+    const mSearch = await auto.toArray();
+    // if (DEBUG) console.log(mSearch);
+    // Render this route with m.search.ejs with the autocomplete.
+    res.render("searchResults/m_search", { mSearch, title: "Mongo Search" });
+    // Event emitter.
+    myEmitter.emit(
+      "log",
+      `Searched: ${req.query.search}\tSTATUS: ${res.statusCode}`,
+      "INFO",
+      "searchLog.log"
+    );
+  } catch (error) {
+    console.error(error);
+    // Send the 503 status code and render 503.ejs to the user.
+    res.status(503).render("503");
+  }
+});
+
+router.get("/postgres", async (req, res) => {
+  try {
+    if (DEBUG) console.log(req.query);
+    let pSearch = await gresSearchData.titleSearch(req.query.search);
+    // if (DEBUG) console.log(pSearch);
+    // Render this route with p.search.ejs with the titleSearch function.
+    res.render("searchResults/p_search", { pSearch, title: "Postgres Search" });
+    // Event emitter.
+    myEmitter.emit(
+      "log",
+      `Searched: ${req.query.search}`,
+      "INFO",
+      "searchLog.log"
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(503).render("503");
+  }
+});
 
 module.exports = router;
