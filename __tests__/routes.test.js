272161e1eb5761b06941f83d97a6208e28e18bc8
csdouce <chris.doucette@keyin.com>
2022-08-18 09:46:06 -0300

    Jest /search/mongo Routes working  w/o emitters

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = new express();
 
 const router = require("../routes/search");
+const router2 = require("../routes/auth");
 const request = require("supertest");
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,8 @@ app.set("view engine", "ejs");
 // const PORT = process.env.PORT || 4000;
 
 app.use("/search", router);
+app.use("/auth", router2);
+
 // app.listen(PORT, "localhost", () => {
 //   console.log(
 //     `Server is running on http://localhost:${PORT}; Ctrl-C to terminate...`
@@ -32,10 +35,41 @@ app.use("/search", router);
 //     .expect("Content-Type", "text/html; charset=utf-8")
 //     .expect(200, done);
 // });
+require("dotenv").config();
+const dal = require("../model/mongo.db.config");
 
-test("responds to /search/mongo", async () => {
-  const res = await request(app).get("/search/mongo/");
-  console.log(res.header);
-  expect(res.header["content-type"]).toMatch(/html/);
-  expect(res.statusCode).toBe(200);
+describe("Testing various routes", () => {
+  beforeAll(async () => {
+    try {
+      await dal.connect();
+      // You actually don't need global here, it works somehow without it, but it makes sense having it there.
+      global.movieCollection = dal.db("sample_mflix").collection("movies");
+      global.userCollection = dal.db("sample_mflix").collection("users");
+      global.commentCollection = dal.db("sample_mflix").collection("comments");
+      global.profileIcon = null;
+      global.DEBUG = true;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  afterAll(async () => {
+    // Close Database here
+    await dal.close();
+  });
+
+  test("responds to /search/mongo", async () => {
+    const res = await request(app).get("/search/mongo?search=Walk");
+    console.log(res.header);
+    expect(res.header["content-type"]).toMatch(/html/);
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("responds to /search/mongo/:_id", async () => {
+    const res = await request(app).get(
+      "/search/mongo/573a1395f29313caabce1282"
+    );
+    expect(res.header["content-type"]).toMatch(/html/);
+    expect(res.statusCode).toBe(200);
+  });
 });
